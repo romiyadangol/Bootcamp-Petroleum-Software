cae75a7754b0d50b80065acf9da9e320f14bab95
romiyadangol <dangolromiya@gmail.com>
2024-08-29 21:20:32 +0545

    Added provider for Redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authReducer from "./reducers/authReducer";
+import orderReducer from "./reducers/orderReducer";
 
 const rootReducer = combineReducers({
     auth: authReducer,
+    order: orderReducer,
 });
 
 const store = configureStore(rootReducer);
