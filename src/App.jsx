8288f079c2dedae1d008989ba619dda35eccbe1f
romiyadangol <dangolromiya@gmail.com>
2024-08-29 16:31:36 +0545

    initial_setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { AgGridReact } from 'ag-grid-react'; 
+import "ag-grid-community/styles/ag-grid.css"; 
+import "ag-grid-community/styles/ag-theme-quartz.css"; 
+import './App.css'
+
+function App() {
+
+  return (
+    <>        
+    </>
+  )
+}
+
+export default App
